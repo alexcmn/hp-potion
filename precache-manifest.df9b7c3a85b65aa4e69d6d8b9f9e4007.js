473671066a9158b30fc53835b65b46cd3852b965
H.Oğuz EKEN <ONE@HOguz-MacBook-Air.local>
2020-01-22 20:51:27 +0100

    Updates

---
+++ precache-manifest.df9b7c3a85b65aa4e69d6d8b9f9e4007.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8de44a9617c3d73b4558a3ae78e8b4c",
+    "url": "/hp-potion.git/index.html"
+  },
+  {
+    "revision": "1c802c9ca4ba21f63ed6",
+    "url": "/hp-potion.git/static/css/main.6942d723.chunk.css"
+  },
+  {
+    "revision": "d670ec80491215ad5e62",
+    "url": "/hp-potion.git/static/js/2.131b2820.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/hp-potion.git/static/js/2.131b2820.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c802c9ca4ba21f63ed6",
+    "url": "/hp-potion.git/static/js/main.03e6824d.chunk.js"
+  },
+  {
+    "revision": "2dc23d41f54ef62cd475",
+    "url": "/hp-potion.git/static/js/runtime-main.a9eab278.js"
+  },
+  {
+    "revision": "1c52ff04a608f1da5a88e9cd8af61445",
+    "url": "/hp-potion.git/static/media/back.1c52ff04.jpg"
+  },
+  {
+    "revision": "5c43bc59ae9cf55beb085a9973875669",
+    "url": "/hp-potion.git/static/media/logo.5c43bc59.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hp-potion.git/precache-manifest.df9b7c3a85b65aa4e69d6d8b9f9e4007.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hp-potion.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
